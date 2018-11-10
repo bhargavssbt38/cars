d6f3eca818cf09af785a6342b6a3f647c6590223
bhargavssbt <bhargavssbt@gmail.com>
2018-11-10 18:53:17 -0500

    employee.java

--- src/users/Employee.java
+++ src/users/Employee.java
@@ -3,6 +3,7 @@ package users;
 import app.*;
 
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class Employee extends User {
 
@@ -52,6 +53,7 @@ public class Employee extends User {
         System.out.println("\nVIEW PROFILE:");
         // TODO: display profile details
        String id=Application.currentUser.userID;
+       System.out.println("ID: "+id);
         String utype=Application.currentUser.userType;
         Application.rs = Application.stmt.executeQuery("select employee.emp_id,employee.emp_name,employee.addr,employee.phone,employee.email,payroll.start_date,payroll.wages,payroll.frequency from employee,payroll where employee.emp_id="+id+"AND employee.emp_id=payroll.emp_id");
 		while (Application.rs.next()) {
@@ -72,7 +74,7 @@ public class Employee extends User {
 		    System.out.println("Wages: " +wages);
 		    System.out.println("Start Date: " +startdate);
 		    System.out.println("frequency: " +frequency);
-		    System.out.println("Role: 1" +utype);
+		    System.out.println("Role: " +utype);
 		}
 
         System.out.println("MENU:");
@@ -116,6 +118,30 @@ public class Employee extends User {
     private void updateProfileField(int field, String newValue) throws Exception {
         // TODO: validate value based on field type
         // TODO: update in db
+    	String id=Application.currentUser.userID;
+    	if(field==1)
+    	{ if(isWord(newValue))
+    	 {Application.stmt.executeUpdate("update employee set emp_name='"+newValue+"' where emp_id="+id);
+    		System.out.println("Profile Details Successfully updated");
+    	 }
+    	else {
+    		System.out.println("Invalid Data");
+    		updateProfile();
+    	}
+    	}
+    	if(field==2) {
+    		
+    	}
+    	
+    }
+    
+    public static boolean validateAddress(String address)
+    {
+    	return address.matches("\\d+\\s+([a-zA-Z]+|[a-zA-Z]+\\s[a-zA-Z]+)");
+    }
+    public static boolean isWord(String in)
+    {
+    	return Pattern.matches("[a-zA-z]+", in);
     }
 
     // Employee: View Customer Profile
@@ -123,10 +149,11 @@ public class Employee extends User {
         System.out.println("\nVIEW CUSTOMER PROFILE:");
         System.out.println("Enter customer email address: ");
         String customerEmail = scanner.next();
+        displayCustomerProfile(customerEmail);
         System.out.println("MENU:");
         System.out.println("\t1. Go back");
         int option = scanner.nextInt();
-        displayCustomerProfile(customerEmail);
+        
         if(option == 1) {
             this.landingPage();
         } else {
@@ -144,7 +171,40 @@ public class Employee extends User {
     //E. Phone Number
     //F. List of All Cars (and their details)
     private void displayCustomerProfile(String customerEmail) throws Exception {
-
+    	Application.rs = Application.stmt.executeQuery("select customer.customer_id,customer.customer_name,customer.email,customer.address,customer.telephone_no, car.license_no, car.car_make,car.car_model,car.car_year,car.customer_id, car.date_of_purchase,car.last_recorded_mileage,car.recent_service_date,recent_service_type from customer,car where customer.email='"+customerEmail+"' AND customer.customer_id=car.customer_id");
+    	//String Query="select customer.customer_id,customer.customer_name,customer.email,customer.address,customer.telephone_no, car.license_no, car.car_make,car.car_model,car.car_year,car.customer_id, car.date_of_purchase,car.last_recorded_mileage,car.recent_service_date,recent_service_type from customer,car where customer.email='"+customerEmail +"' AND customer.customer_id=car.customer_id";
+    	//System.out.println(Query);
+    	while (Application.rs.next()) {
+		    int cid= Application.rs.getInt("customer_id");
+		    String name = Application.rs.getString("customer_name");
+		    String addr = Application.rs.getString("address");
+		    String phone = Application.rs.getString("telephone_no");
+		    String license_no = Application.rs.getString("license_no");
+		    String email= Application.rs.getString("email");
+		    String car_make = Application.rs.getString("car_make");
+		    String car_year = Application.rs.getString("car_year");
+		    Date purchasedate=Application.rs.getDate("date_of_purchase");
+		    String car_model = Application.rs.getString("car_model");
+		    int mileage = Application.rs.getInt("last_recorded_mileage");
+		    Date latest_service_date = Application.rs.getDate("recent_service_date");
+		    String recent_service_type = Application.rs.getString("recent_service_type");
+		    
+		    
+		    System.out.println("Customer ID: "+cid);
+		    System.out.println("Customer Name: " +name);
+		    System.out.println("Address: " +addr);
+		    System.out.println("Phone: " +phone);
+		    System.out.println("email: " +email);
+		    System.out.println("\n\nCar Details");
+		    System.out.println("license no: " +license_no);
+		    System.out.println("Car_Make:  " +car_make);
+		    System.out.println("Car Year " +car_year);
+		    System.out.println("Purchase Date:" +purchasedate);
+		    System.out.println("Car Model: " +car_model);
+		    System.out.println("Last recorder mileage: " +mileage);
+		    System.out.println("latest_service_date: " +latest_service_date);
+		    System.out.println("Recent Service Type: " +recent_service_type);
+		}
     }
 
 }
